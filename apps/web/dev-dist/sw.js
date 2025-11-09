d4b030340b80f4a3e78511bb99f5b4ba578b6152
digoburigo <rodrigobesmeraldino@gmail.com>
2025-11-09 15:26:28 -0300

    chore: copy and fi

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a702e535'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nla3inqsktk"
+    "revision": "0.a0s9eks3q8c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
